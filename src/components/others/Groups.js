62bb269358ae043886944db391f6bc5c52265b1f
mohamedalijinnah81 <mohamedalijinna56@gmail.com>
2024-02-22 18:41:55 +0530

    Some animations added

--- src/components/others/Groups.js
+++ src/components/others/Groups.js
@@ -8,68 +8,71 @@ import '../../styles/DarkTheme.css'
 import logo from '../../images/live-chat.png'
 import SBSearchbar from '../sidebar/SBSearchbar'
 import { useSelector } from 'react-redux'
+import { AnimatePresence, motion } from "framer-motion"
 
 const Groups = () => {
     const lightTheme = useSelector((state) => state.themeKey);
 
   return (
-    <div className='list-container'>
-        <div className={'ug-header' + ((lightTheme) ? '' : ' dark')}>
-            <img src={logo} alt="Logo" style={{height: "2rem", width: "2rem", marginLeft: "10px"}} />
-            <p className='ug-title'>Available Groups</p>
-        </div>
-        <SBSearchbar />
-        <div className="ug-list">
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
+    <AnimatePresence>
+        <motion.div 
+            initial={{opacity: 0, scale: 0}} 
+            animate={{opacity: 1, scale: 1}} 
+            exit={{opacity: 0, scale: 0}} 
+            transition={{
+                ease: "anticipate",
+                duration: "0.3"
+            }} 
+            className='list-container'>
+            <div className={'ug-header' + ((lightTheme) ? '' : ' dark')}>
+                <img src={logo} alt="Logo" style={{height: "2rem", width: "2rem", marginLeft: "10px"}} />
+                <p className='ug-title'>Available Groups</p>
             </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
+            <SBSearchbar />
+            <div className="ug-list">
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>N</p>
+                    <p className='convo-title'>New Group</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>N</p>
+                    <p className='convo-title'>New Group</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>N</p>
+                    <p className='convo-title'>New Group</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>N</p>
+                    <p className='convo-title'>New Group</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>N</p>
+                    <p className='convo-title'>New Group</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>N</p>
+                    <p className='convo-title'>New Group</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>N</p>
+                    <p className='convo-title'>New Group</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>N</p>
+                    <p className='convo-title'>New Group</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>N</p>
+                    <p className='convo-title'>New Group</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>N</p>
+                    <p className='convo-title'>New Group</p>
+                </motion.div>
             </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>N</p>
-                <p className='convo-title'>New Group</p>
-            </div>
-        </div>
-    </div>
+        </motion.div>
+    </AnimatePresence>
   )
 }
 
--- src/components/others/Users.js
+++ src/components/others/Users.js
@@ -9,68 +9,72 @@ import '../../styles/DarkTheme.css'
 import logo from '../../images/live-chat.png'
 import SBSearchbar from '../sidebar/SBSearchbar'
 import { useSelector } from 'react-redux'
+import { AnimatePresence, motion } from "framer-motion"
 
 const Users = () => {
     const lightTheme = useSelector((state) => state.themeKey);
 
   return (
-    <div className='list-container'>
-        <div className={'ug-header' + ((lightTheme) ? '' : ' dark')}>
-            <img src={logo} alt="Logo" style={{height: "2rem", width: "2rem", marginLeft: "10px"}} />
-            <p className='ug-title'>Online Users</p>
-        </div>
-        <SBSearchbar />
-        <div className="ug-list">
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
+    <AnimatePresence>
+        <motion.div 
+            initial={{opacity: 0, scale: 0}} 
+            animate={{opacity: 1, scale: 1}} 
+            exit={{opacity: 0, scale: 0}} 
+            transition={{
+                ease: "anticipate",
+                duration: "0.3"
+            }} 
+            className='list-container'>
+            <div className={'ug-header' + ((lightTheme) ? '' : ' dark')}>
+                <img src={logo} alt="Logo" style={{height: "2rem", width: "2rem", marginLeft: "10px"}} />
+                <p className='ug-title'>Online Users</p>
             </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
+            <SBSearchbar />
+            <div className="ug-list">
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>T</p>
+                    <p className='convo-title'>Temp User</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>T</p>
+                    <p className='convo-title'>Temp User</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>T</p>
+                    <p className='convo-title'>Temp User</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>T</p>
+                    <p className='convo-title'>Temp User</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>T</p>
+                    <p className='convo-title'>Temp User</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>T</p>
+                    <p className='convo-title'>Temp User</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>T</p>
+                    <p className='convo-title'>Temp User</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>T</p>
+                    <p className='convo-title'>Temp User</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>T</p>
+                    <p className='convo-title'>Temp User</p>
+                </motion.div>
+                <motion.div whileHover={{scale: 1.01}} whileTap={{scale: 0.98}} className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
+                    <p className='convo-icon'>T</p>
+                    <p className='convo-title'>Temp User</p>
+                </motion.div>
             </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
-            </div>
-            <div className={'ug-listitem' + ((lightTheme) ? '' : ' dark')}>
-                <p className='convo-icon'>T</p>
-                <p className='convo-title'>Temp User</p>
-            </div>
-        </div>
-    </div>
+        </motion.div>
+    </AnimatePresence>
+    
   )
 }
 
